34e395a0fad235ac3d16ffc297080b4024380199
Youssefwmf <youssefmahartiste@gmail.com>
2025-06-09 04:18:05 +0100

    ci/cd dummy test

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ class Location(BaseModel):
 
 def search_shops(lat: float, lng: float):
     return [
-        {"name": "Example Shop A", "lat": lat + 0.001, "lng": lng + 0.001},
+        {"name": "Example Shop A", "lat": lat + 0.006, "lng": lng + 0.001},
         {"name": "Example Shop B", "lat": lat - 0.001, "lng": lng - 0.001},
     ]
 
 @app.post("/shops")
-def get_shops(loc: Location):
+def get_shops(loc: Location)
     """
     loc is parsed from the JSON body into a Location object.
     We call search_shops and return its result as JSON.
